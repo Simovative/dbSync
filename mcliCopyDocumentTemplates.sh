69d1fdcb375a984100d593b5065bbe65e50513cf
Stefan Luecke <stefan.luecke@simovative.com>
2024-06-06 09:47:14 +0200

    PLAT-6174 Check wether mcli or mc is our minio client executable. Furthermore I added a note about a mc bug which prevents the script from working.

--- mcliCopyDocumentTemplates.sh
+++ mcliCopyDocumentTemplates.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "There is currently a minio client bug preventing this script from working."
+echo "Bug Reference: https://github.com/minio/mc/issues/4893"
+exit -1
+
 usage() {
     echo "Usage: $0 -s <source_config> -d <destination_config>"
     echo "  -s  Source S3 configuration directory path"
@@ -20,6 +24,18 @@ if [ -z "$SOURCE_CONFIG" ] || [ -z "$DEST_CONFIG" ]; then
     usage
 fi
 
+if command -v mcli &> /dev/null; then
+    MC="mcli"
+elif command -v mc &> /dev/null; then
+    VERSION=$(mc --version 2>&1)
+
+    if echo "$VERSION" | grep -q "MinIO Client"; then
+	MC="mc"
+    else
+	echo "Could not find MinIO Client. Please make sure it is installed and run the script again."
+    fi
+fi
+
 TMP_DIR=$(mktemp -d)
 mkdir -p "${TMP_DIR}/documenttemplates"
 mkdir -p "${TMP_DIR}/document_files"
@@ -35,14 +51,14 @@ trap cleanup EXIT
 BUCKET_PREFIX="ac5bucket/mycustomeridentifier"
 
 echo "Copying document templates to temporary directory..."
-mcli -r -C "$SOURCE_CONFIG" get s3://"${BUCKET_PREFIX}/documenttemplates" "${TMP_DIR}/documenttemplates"
+$MC -C "$SOURCE_CONFIG" cp -r s3://"${BUCKET_PREFIX}/documenttemplates" "${TMP_DIR}/documenttemplates"
 if [ $? -ne 0 ]; then
     echo "Error copying document templates from source bucket to temporary directory."
     exit 1
 fi
 
 echo "Copying document files to temporary directory..."
-mcli -r -C "$SOURCE_CONFIG" get s3://"${BUCKET_PREFIX}/document_files" "${TMP_DIR}/documentfiles"
+$MC -C "$SOURCE_CONFIG" cp -r s3://"${BUCKET_PREFIX}/document_files" "${TMP_DIR}/documentfiles"
 if [ $? -ne 0 ]; then
     echo "Error copying document files from source bucket to temporary directory."
     exit 1
@@ -50,14 +66,14 @@ fi
 
 
 echo "Copying document templates to destination bucket..."
-mcli -r -C "$DEST_CONFIG" put "${TMP_DIR}/documenttemplates" s3://"${BUCKET_PREFIX}/documenttemplates/"
+$MC -C "$DEST_CONFIG" cp -r "${TMP_DIR}/documenttemplates" s3://"${BUCKET_PREFIX}/documenttemplates/"
 if [ $? -ne 0 ]; then
     echo "Error copying document templates from temporary directory to destination bucket."
     exit 1
 fi
 
 echo "Copying document files to destination bucket..."
-mcli -r -C "$DEST_CONFIG" put "${TMP_DIR}/document_files" s3://"${BUCKET_PREFIX}/document_files/"
+$MC -C "$DEST_CONFIG" cp -r "${TMP_DIR}/document_files" s3://"${BUCKET_PREFIX}/document_files/"
 if [ $? -ne 0 ]; then
     echo "Error copying document files from temporary directory to destination bucket."
     exit 1
