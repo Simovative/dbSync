4098a54b647f6d873ecf6b5b181f1a404944d272
Stefan Luecke <stefan.luecke@simovative.com>
2024-06-12 11:30:24 +0200

    PLAT-6174 Removed mcli version of script, made customer identifier variable and removed document_files

--- s3cmdCopyDocumentTemplates.sh
+++ s3cmdCopyDocumentTemplates.sh
@@ -4,25 +4,26 @@ usage() {
     echo "Usage: $0 -s <source_config> -d <destination_config>"
     echo "  -s  Source S3 configuration file path"
     echo "  -d  Destination S3 configuration file path"
+    echo "  -i  Customer identifier"
     exit 1
 }
 
-while getopts ":s:d:" opt; do
+while getopts ":s:d:i:" opt; do
     case $opt in
         s) SOURCE_CONFIG=$OPTARG ;;
         d) DEST_CONFIG=$OPTARG ;;
+	i) IDENTIFIER=$OPTARG ;;
         \?) echo "Invalid option: -$OPTARG" >&2; usage ;;
         :) echo "Option -$OPTARG requires an argument." >&2; usage ;;
     esac
 done
 
-if [ -z "$SOURCE_CONFIG" ] || [ -z "$DEST_CONFIG" ]; then
+if [ -z "$SOURCE_CONFIG" ] || [ -z "$DEST_CONFIG" ] || [ -z "$IDENTIFIER" ]; then
     usage
 fi
 
 TMP_DIR=$(mktemp -d)
 mkdir -p "${TMP_DIR}/documenttemplates"
-mkdir -p "${TMP_DIR}/document_files"
 
 cleanup() {
     echo "Removing temporary directory"
@@ -32,7 +33,7 @@ cleanup() {
 # Trap to ensure cleanup is done on exit
 trap cleanup EXIT
 
-BUCKET_PREFIX="ac5bucket/mycustomeridentifier"
+BUCKET_PREFIX="ac5bucket/${IDENTIFIER}"
 
 echo "Copying document templates to temporary directory..."
 s3cmd -r -c "$SOURCE_CONFIG" get s3://"${BUCKET_PREFIX}/documenttemplates" "${TMP_DIR}/documenttemplates"
@@ -41,25 +42,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Copying document files to temporary directory..."
-s3cmd -r -c "$SOURCE_CONFIG" get s3://"${BUCKET_PREFIX}/document_files" "${TMP_DIR}/documentfiles"
-if [ $? -ne 0 ]; then
-    echo "Error copying document files from source bucket to temporary directory."
-    exit 1
-fi
-
-
 echo "Copying document templates to destination bucket..."
 s3cmd -r -c "$DEST_CONFIG" put "${TMP_DIR}/documenttemplates" s3://"${BUCKET_PREFIX}/documenttemplates/"
 if [ $? -ne 0 ]; then
     echo "Error copying document templates from temporary directory to destination bucket."
-    exit 1
-fi
-
-echo "Copying document files to destination bucket..."
-s3cmd -r -c "$DEST_CONFIG" put "${TMP_DIR}/document_files" s3://"${BUCKET_PREFIX}/document_files/"
-if [ $? -ne 0 ]; then
-    echo "Error copying document files from temporary directory to destination bucket."
     exit 1
 fi
 
